326ff6b0e4aabfeec1586ad5c8b88db5beea31e0
Artem <kot7672@gmail.com>
2019-11-24 23:25:00 +0300

    Фикс соединения с БД

--- download_spider.py
+++ download_spider.py
@@ -6,17 +6,15 @@ from sqlite3 import connect
 
 class DownloadSpider(USpider):
     file = None
-    start_urls = []
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.start_urls.extend(load_links_from_json(self.file))
+        self.start_urls = load_links_from_json(self.file)
 
     def parse(self, response):
         text = extract(response.body)
         if self.extract_text == "db":
-            conn = connect(f"{self.name}.db")
-            cursor = conn.cursor()
+            cursor = self.conn.cursor()
             cursor.execute(f"""
             INSERT OR REPLACE INTO texts (url, extracted_text) 
             VALUES ('{response.url}', '{text.replace("'", '"')}')
